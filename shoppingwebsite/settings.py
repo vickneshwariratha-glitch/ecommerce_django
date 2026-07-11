0b4676117a94712535b078dccf9cdae2a43bdaff
vickneshwariratha-glitch <vickneshwariratha@gmail.com>
2026-07-11 11:18:19 +0530

    Update settings.py

--- shoppingwebsite/settings.py
+++ shoppingwebsite/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-uqy^^ro2*71i%qzo0aj^aeqk!8v*^@s+*^bm+yr%fgwwu21h55
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
